efc144a4e7afba4ab20d4201ec58f934f1e59152
luggieman <cdjaffier@hotmail.com>
2016-08-20 00:48:02 -0400

    alert function added

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,10 @@ var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
+
+if (cardTwo === cardFour) {
+	alert("You found a match!");
+}
+else if (cardTwo !== cardFour) {
+	alert("Sorry, try again");
+}
