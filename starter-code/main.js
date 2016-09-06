4e844d53c0f660b4bda8e8d9f0d9a76d47fc9122
luggieman <cdjaffier@hotmail.com>
2016-09-06 00:36:21 -0400

    added card creation logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,9 +4,28 @@ var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
 
-if (cardTwo === cardFour) {
-	alert("You found a match!");
+//if (cardTwo === cardFour) {
+	//alert("You found a match!");
+//}
+//else if (cardTwo !== cardFour) {
+	//alert("Sorry, try again");
+//}
+
+
+for (var i = 0; i < 4; i++) {
+  var card = document.createElement('div');
+  card.className = 'card';
+  card.appendChild(document.createTextNode("New Card"));
+  document.getElementById("game-board")
+  .appendChild(card);
 }
-else if (cardTwo !== cardFour) {
-	alert("Sorry, try again");
+
+function createBoard () {
+	for (var i = 0; i < 4; i++) {
+  var card = document.createElement('div');
+  card.className = 'card';
+  card.appendChild(document.createTextNode("New Card"));
+  document.getElementById("game-board")
+  .appendChild(card);
+	}
 }
