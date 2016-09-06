435b0e7f4549e19f88b7471cde1647dd1bfed14e
luggieman <cdjaffier@hotmail.com>
2016-09-06 01:56:54 -0400

    added king and queen attributes

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,31 +1,46 @@
-
 var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
-
 //if (cardTwo === cardFour) {
-	//alert("You found a match!");
+//alert("You found a match!");
 //}
 //else if (cardTwo !== cardFour) {
-	//alert("Sorry, try again");
+//alert("Sorry, try again");
 //}
-
-
 for (var i = 0; i < 4; i++) {
-  var card = document.createElement('div');
-  card.className = 'card';
-  card.appendChild(document.createTextNode("New Card"));
-  document.getElementById("game-board")
-  .appendChild(card);
+    var card = document.createElement('div');
+    card.className = 'card';
+    card.appendChild(document.createTextNode("New Card"));
+    document.getElementById("game-board").appendChild(card);
 }
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
-function createBoard () {
-	for (var i = 0; i < 4; i++) {
-  var card = document.createElement('div');
-  card.className = 'card';
-  card.appendChild(document.createTextNode("New Card"));
-  document.getElementById("game-board")
-  .appendChild(card);
-	}
-}
+function createBoard() {
+        for (var i = 0; i < 4; i++) {
+            var card = document.createElement('div');
+            card.className = 'card';
+            card.appendChild(document.createTextNode("New Card"));
+            document.getElementById("game-board").appendChild(card);
+        }
+        for (var i = 0; i < cards.length; i++) {
+            cardElement.setAttribute('king', cards[i]);
+            cardElement.setAttribute('queen', cards[i]);
+            board.appendChild(cardElement);
+    		board.appendChild(cardElement);
+        }
+        for (var i = 0; i < cards.length; i++) {
+            cardElement.addEventListener('click', isTwoCards)
+        }
+
+        function isTwoCards() {
+            cardsInPlay.push(this.getAttribute('data-card'));
+            if (cardsInPlay.length === 2) {
+                isMatch(cardsInPlay);
+                cardsInPlay = [];
+            }
+
+          }
+      }
+        
